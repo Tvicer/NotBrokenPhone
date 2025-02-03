04047caf91488434de37097af0f6ec8809f476f3
Носов Александр Сергеевич <asnosov@phoenixit.ru>
2025-02-03 11:07:34 +0300

    init commit

---
+++ src/main/java/com/notbrokenphone/notbrokenphone/NotBrokenPhoneApplication.java
@@ -0,0 +1,13 @@
+package com.notbrokenphone.notbrokenphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotBrokenPhoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotBrokenPhoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/notbrokenphone/notbrokenphone/NotBrokenPhoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notbrokenphone.notbrokenphone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotBrokenPhoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
